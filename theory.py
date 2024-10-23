30445103f997248d8cfafba9119f191916be43ed
John Feng <johnfengphd@gmail.com>
2024-10-23 13:26:36 -0700

    module to store equations

---
+++ theory.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Tungsten PETG
+DENSITY = 4.0 #g/cm^3 according to Prusa's article
+
+def Intensity(x, I0, mu, offset):
+    return I0 * np.exp(-mu * x) + offset
+
+def HVL(mu):
+    # ln(2)/mu
+    return np.log(2)/mu 
+
+def T_mean_to_mu(T_mean, x):
+    return -np.log(T_mean)/x
+
+def MFP(mu):
+    return 1/mu
+
+def mass_mu(mu, DENSITY):
+    return mu/DENSITY
+
